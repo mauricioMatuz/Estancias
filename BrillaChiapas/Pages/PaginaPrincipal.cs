dd8fa26f689067277c4fc7b434a086627f667fe5
mauricio matuz tamayo <killashe96@gmail.com>
2021-02-08 09:21:32 -0600

    Agregar archivos de proyecto.

---
+++ BrillaChiapas/Pages/PaginaPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrillaChiapas.Pages
+{
+    public partial class PaginaPrincipal
+    {
+        private int score = 0;
+        private int currentTime = 20;
+        private int random;
+        int hitPosicion = 0;
+        private String message = " ";
+        bool gameIsRunning = true;
+
+    }
+}
