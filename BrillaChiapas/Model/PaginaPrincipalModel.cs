dae06d0ab872bba2dfb9d383b1712db50867a7b9
mauricio matuz tamayo <killashe96@gmail.com>
2021-02-09 10:23:35 -0600

    agregue sonido de acierto e imagen tachado

---
+++ BrillaChiapas/Model/PaginaPrincipalModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrillaChiapas.Model
+{
+    public class PaginaPrincipalModel
+    {
+        private bool inicio;
+        private bool acerto;
+        public int id
+        {
+            set;get;
+        }
+        public String style
+        {
+            set; get;
+        }
+
+        public bool Inicio
+        {
+            get => inicio;
+            set
+            {
+                inicio = value;
+                if (inicio)
+                {
+                    style = "mole";
+                }
+                else
+                {
+                    style = "";
+                }
+            }
+        }
+        public bool Acerto
+        {
+            get => acerto;
+            set
+            {
+                acerto = value;
+                if (acerto)
+                {
+                    style = "acerto";
+                }
+                else
+                {
+                    style = "";
+                }
+            }
+        }
+        public class Sonido
+        {
+            public bool reproduciendo { get; set; }
+          public String rutaSonido { get; set; }
+        }
+        
+        Sonido[] sonidos = new Sonido[]
+        {
+        new Sonido{ reproduciendo = false, rutaSonido = "sound/correcto.wav"},
+        };
+
+    }
+}
--- BrillaChiapas/Pages/PaginaPrincipal.cs
+++ BrillaChiapas/Pages/PaginaPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrillaChiapas.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +14,69 @@ namespace BrillaChiapas.Pages
         int hitPosicion = 0;
         private String message = " ";
         bool gameIsRunning = true;
+        public List<PaginaPrincipalModel> matriz { get; set; } = new List<PaginaPrincipalModel>();
+        int posicionPrd = new Random().Next(0, 12);
+        public PaginaPrincipal()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                //for(int x = 0; x < 6; x++)
+                matriz.Add(new PaginaPrincipalModel
+                {
+                    id = i
+                });
+            }
+        }
 
+        private void MouseAction(PaginaPrincipalModel model)
+        {
+            Console.WriteLine("valor de random " + posicionPrd);
+            foreach (var item in matriz)
+            {
+                item.Acerto = false;
+            }
+            if (model.id == hitPosicion)
+            {
+                matriz[posicionPrd] = true;
+                score += 1;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void MovImagen()
+        {
+            foreach (var item in matriz)
+            {
+                item.Inicio = false;
+            }
+           
+            matriz[posicionPrd].Inicio = true;
+            hitPosicion = posicionPrd;
+            StateHasChanged();
+        }
+
+        private async Task game()
+        {
+            while (gameIsRunning)
+            {
+                currentTime--;
+                if (currentTime == 0)
+                {
+                    message = "FIN DEL JUEGO";
+                    gameIsRunning = false;
+                }
+                MovImagen();
+                await Task.Delay(1000);
+            }
+        }
+
+        protected override async void OnInitialized()
+        {
+            await game();
+
+        }
     }
 }
